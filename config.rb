98d5b69b79586e4d0a418e03673818f13d4062e4
Malte Bublitz <bublitz@dma.do>
2014-02-18 09:15:02 +0100

    Switched to SCSS.

---
+++ config.rb
@@ -0,0 +1,19 @@
+#
+# Compass configuration
+#
+
+http_path = "/"
+css_dir = "webroot/assets/css"
+sass_dir = "webroot/assets/scss"
+images_dir = "images"
+javascripts_dir = "js"
+
+# Possible values:
+#  - :expanded
+#  - :nested
+#  - :compact
+#  - :compressed
+output_style = :compressed
+
+# No debugging comments
+line_comments = false
